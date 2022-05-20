771014abcef255b5f6c085b1f9495bde6a566ab4
tiamed <tiamedue@gmail.com>
2022-05-21 05:46:39 +0800

    build: postbuild

--- compile/postbuild.js
+++ compile/postbuild.js
@@ -1,5 +1,5 @@
 import * as fs from 'fs';
 import path from 'path';
-import main from '../build/src/main.js';
+import main from '../build/main.js';
 
 fs.writeFileSync(path.resolve('build/main.json'), JSON.stringify(main, null, 2));
